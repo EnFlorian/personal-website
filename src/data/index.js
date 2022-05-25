9acdc7c92368e3dee98271f276dc26222d1ec253
unknown <33962956+EnFlorian@users.noreply.github.com>
2022-05-25 06:06:41 +0200

    Added link data

---
+++ src/data/index.js
@@ -0,0 +1,27 @@
+export const navItems = [
+  {
+    id: "home",
+    title: "Home",
+    link: "/",
+  },
+  {
+    id: "about",
+    title: "About",
+    link: "#about",
+  },
+  {
+    id: "experience",
+    title: "Experience",
+    link: "#experience",
+  },
+  {
+    id: "portfolio",
+    title: "Portfolio",
+    link: "#portfolio",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+    link: "#contact",
+  },
+];
